17c02d27fae79ef87f748ad8878ee68198eb62da
Bernard Amankwah <bernardamankwah@hotmail.com>
2023-09-28 14:06:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,120 @@
+// contenitore = valore
+
+//ho scritto un console log per dire ciao
+
+// let
+// const
+
+// STRING NUMBER BOOLEAN NULL E UNDEFINED
+
+let car = "fiat";
+// console.log(car);
+// car = 5;
+
+// console.log(car);
+
+const newCar = "bmw";
+
+// console.log(newCar);
+
+// let number = 5;
+// console.log(number);
+
+// let value = true;
+// let value2 = false;
+
+// let nullVal = null;
+// let noDefine;
+
+// console.log(typeof nullVal);
+// console.log(noDefine);
+
+// let boolCheck = 0
+
+// console.log(typeof boolCheck);
+
+// let helloWorld = 'x'
+
+// let x = 5;
+// let y = 6;
+
+// console.log(x > y);
+
+// Create four variables (brand, model, production year, owner's name) and assign them a value.
+// Use let and const where appropriate.
+// Create a variable to define if a car is new or not new.
+// Use the number, boolean and string data type to create the variables.
+// If you use a name with more than one word, remember to use the camel case naming convention.
+
+// const brand = "fiat";
+// const model = "punto";
+// const production_year = 2014;
+// let ownersName = "mario";
+
+// let isNew = true;
+
+// Write a better names for variables where it's necessary.
+
+// let _name = 'mario';
+// let surname = 'Rossi';
+// let age = 20;
+// let isMarried = false;
+// let job = 'developer';
+// let isGraduated2 = false;
+
+// Create the variables firstName, lastName and fullName.
+// Assign them a value and print in the console the full name
+
+// const firstName = 'Anna'
+// const lastName = 'Simionato'
+
+// let fullName = `il mio nome è ${firstName} ${lastName}`
+
+// console.log(fullName);
+
+//CONVERTING TYPES
+
+// let nullValue = null;
+// let string = "null";
+// let number = 9;
+// let boolean = true;
+// let newNumber = -1;
+
+// console.log(Boolean(newNumber));
+// console.log(typeof boolean);
+
+// let x = 0;
+// x = Boolean(x);
+
+// console.log(x);
+
+// Convert the hello variable to a boolean data type.
+// Convert the age variable to a string data type.
+// Convert the isGraduated variable to a numeric data type
+
+// let hello = 'Ciao'; // boolean
+// let age = 18; // string
+// let isGraduated = false; // number
+
+// hello = Boolean(hello)
+// age = String(age)
+// isGraduated = Number(isGraduated)
+
+// console.log(hello, age, isGraduated);
+
+// Create a variable and set it to true.
+// Print the value in the console and the data type. typeof()
+// Convert it to a numeric data type, print the value and the data type.
+// Convert it to string data type, print the value and the data type.
+// Finally, convert it to a boolean data type, print the value and the data type.
+
+let x = true;
+
+// console.log("variable", x);
+// console.log("type of variable is", typeof x);
+
+// let y = Number(x);
+// y = String(y);
+// y = Boolean(y);
+
+// console.log(typeof y);
